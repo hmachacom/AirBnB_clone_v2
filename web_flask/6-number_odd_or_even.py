c86a39dfa24f81b9021fcb76e1dc15e24ad53454
Hugo Machacon <hmachacom@gmail.com>
2022-05-24 15:35:55 -0500

    6-number_odd_or_even.py templates/6-number_odd_or_even.html

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -97,7 +97,9 @@ def n_is_integer_html_odd_even(n):
             even_odd = "even"
         else:
             even_odd = "odd"
-        return render_template('6-number_odd_or_even.html', number=n, even_odd=even_odd)
+        return render_template(
+            '6-number_odd_or_even.html', number=n, even_odd=even_odd
+            )
 
 
 if __name__ == "__main__":
