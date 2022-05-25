7680849b40d93cff4d17206ebd8be498cc3c773e
Hugo Machacon <hmachacom@gmail.com>
2022-05-25 09:55:59 -0500

    add class delet

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,14 +3,9 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def close_storage():
-    """request you must remove the current SQLAlchemy Session"""
-    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
@@ -18,6 +13,12 @@ def states_list():
     """display a HTML page: (inside the tag BODY)"""
     states = storage.all(State).values()
     return(render_template("7-states_list.html", states=states))
+
+
+@app.teardown_appcontext
+def close_storage_new(self):
+    """request you must remove the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == "__main__":
